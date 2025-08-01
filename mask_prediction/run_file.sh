9597abe337ce965f6c4c486ca833188e547035e7
Amartya Hatua <amartyahatua@gmail.com>
2025-08-01 09:56:20 -0400

    Updating

--- mask_prediction/run_file.sh
+++
@@ -1,3 +0,0 @@
-python drive.py --train_path "/content/drive/MyDrive/PLM/SwissprotDatasets/BalancedSwissprot/train.csv" \
-                         --test_path "/content/drive/MyDrive/PLM/SwissprotDatasets/BalancedSwissprot/valid.csv" \
-                         --epochs 5

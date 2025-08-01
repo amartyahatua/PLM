df51ee32b32ef52a4dad0abcae94605905ca0b5f
Amartya Hatua <amartyahatua@gmail.com>
2025-08-01 09:51:13 -0400

    Deleting

--- mask_prediction/mask_model/hp_finetune.py
+++
@@ -1,7 +0,0 @@
-def hp_space(trial):
-    return {
-        "learning_rate": trial.suggest_float("learning_rate", 1e-5, 5e-4, log=True),
-        "num_train_epochs": trial.suggest_int("num_train_epochs", 2, 5),
-        "per_device_train_batch_size": trial.suggest_categorical("per_device_train_batch_size", [8, 16, 32]),
-        "weight_decay": trial.suggest_float("weight_decay", 0.0, 0.1),
-    }
